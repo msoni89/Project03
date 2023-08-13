4c3df34bd5c6718d56bb5d9307542616aa940b02
Mehul Soni <mehul.soni.estonia@gmail.com>
2023-08-13 15:46:10 +0300

    fixed.. added searching into sorted rotated array

--- src/main/java/org/project/problem/SearchInRotatedSortedArray.java
+++ src/main/java/org/project/problem/SearchInRotatedSortedArray.java
@@ -28,22 +28,6 @@ public class SearchInRotatedSortedArray {
                 }
             }
         }
-
-
-        // 4,5,6,7,0,1,2
-//        if (nums[l] <= nums[mid]) {
-//            if (nums[mid] < target || target < nums[l]) {
-//                l = mid + 1;
-//            } else {
-//                r = mid - 1;
-//            }
-//        } else {
-//            if (nums[mid] > target || target > nums[r]) {
-//                r = -1;
-//            } else {
-//                l = mid + 1;
-//            }
-//        }
         return -1;
     }
 }
